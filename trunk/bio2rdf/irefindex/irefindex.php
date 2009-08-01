4a4a43297c2aa017b34cf96aa5c7ae37c790654b
michel.dumontier <michel.dumontier@d60594c4-dda9-11dd-87d8-31aa04531ed5>
2009-08-01 16:58:04 +0000

    Reorganized code

--- trunk/bio2rdf/irefindex/irefindex.php
+++ trunk/bio2rdf/irefindex/irefindex.php
@@ -27,7 +27,7 @@ function irefindex($infp, $outfp)
 	include ('../include.php');
 	$buf = N3NSHeader($nslist);
 	$oids = '';
-	
+		
 	$nsmap = array(
 		'emb' => 'embl',
 		'gb' => 'ncbi',
@@ -38,82 +38,112 @@ function irefindex($infp, $outfp)
 		'kegg:ecj' => 'kegg'
 	);
 	
-	
-	// MI:0314 - Complex
-	// 
+
 	$z = 0;
+	$liid = ''; // last interaction id
 	fgets($infp);
 	while($l = fgets($infp)) {
 		$a = explode("\t",trim($l));
 		
-		$type = $a[11]=="NA"?"ss:Complex":$a[11];
-		
 		$ids = explode("|",$a[13]);
-		$iid = $ids[0];
-
+		$iid = $ids[0]; // the irefindex indentifier
 		$buf .= "$iid dc:identifier \"$iid\".".PHP_EOL;
-		$rel = $a[20];
-		if($rel == 'Y') {
-			$homodimer = true;
-			// homodimer 
-			$buf .= "$iid a ss:SelfInteraction.".PHP_EOL;
-		} else if($rel == 'C') {
-			// complex
-			$buf .= "$iid a ss:Complex.".PHP_EOL;
-		} else {
-			$buf .= "$iid a ss:Interaction.".PHP_EOL;
+		if(!$liid) $liid = $iid; // first setting
+		// process the other ids
+		array_shift($ids);
+		foreach($ids AS $id) {
+			BreakName($id, &$id_ns, &$id, &$id_label);
+			$buf .= "$iid owl:sameAs $id_ns:$id .".PHP_EOL;
 		}
-
-		BreakName($a[17], &$ns1, &$id1, &$label1);
-		BreakName($a[18], &$ns2, &$id2, &$label2);
-		
-	
-		BreakName($a[11],  &$ns4, &$itypeid, &$itlabel);
-		$itype = 'interaction';
-		if($ns4 != '' && $itypeid != 'NA') {
-			$buf .= "$iid rdf:type $ns4:$itypeid .".PHP_EOL;
-			$mlabel = "$itlabel";
-			$oids["$ns4:$itypeid"] = $itlabel;
+			
+		// MI-specified interaction type
+		BreakName($a[11], &$it_ns, &$it_id, &$it_label);
+		if($it_ns != '' && $it_id != 'NA') {
+			$buf .= "$iid a $it_ns:$it_id .".PHP_EOL;
+			$oids["$it_ns:$it_id"] = $it_label; // keep a list of MI entities to generate labels
 		}
 		
-		BreakName($a[6],  &$ns3, &$mid, &$ml);
-		$mlabel = '';
+		// MI-specified method
+		BreakName($a[6],  &$m_ns, &$m_id, &$m_label);
 		if($mid != '0000') {
-			$buf .= "$iid bio2rdf:method $ns3:$mid .".PHP_EOL;
-			$mlabel = "identified by $ml";
-			$oids["$ns3:$mid"] = $ml;
-		}
-		
-		$buf .= "$iid rdfs:label \"$label1-$label2 $itype between $a[2] and $a[3] $mlabel [$iid]\".".PHP_EOL;
-		foreach($ids AS $idstring) {
-			$buf .= "$iid owl:sameAs $idstring .".PHP_EOL;
-		}
-		
-		$buf .= "$iid bio2rdf:component $a[0].".PHP_EOL;
-		
-		$buf .= "$a[0] bio2rdf:interactsWith $a[1].".PHP_EOL;
-		BreakName($type,$ns,$id,$label);
-		$buf .= "$iid a $ns:$id.".PHP_EOL;
-		
-		$c = explode("|",$a[2]);
-		foreach($c as $lid) {
-			BreakName($lid,$ns,$id,$label);
-			if(isset($nsmap[$ns])) $ns = $nsmap[$ns];
-			$buf .= "$a[0] owl:sameAs $ns:$id.".PHP_EOL;
-		}
-		if($a[9] != 'NA') $buf .= "$a[0] bio2rdf:organism ".str_replace("taxid","taxon",$a[9]).".".PHP_EOL;
-		if(!$homodimer) {
-			$buf .= "$iid bio2rdf:component $a[1].".PHP_EOL;
-			$c = explode("|",$a[3]);
-			foreach($c as $lid) {
-				BreakName($lid,$ns,$id,$label);
-				if(isset($nsmap[$ns])) $ns = $nsmap[$ns];
-				$buf .= "$a[1] owl:sameAs $ns:$id.".PHP_EOL;
+			$buf .= "$iid bio2rdf:method $m_ns:$m_id .".PHP_EOL;
+			if($m_label == '-1') $m_label = 'by unspecified method';
+			else {
+				$oids["$m_ns:$m_id"] = $m_label;
+				$m_label = "identified by $m_label"; // for label generation
 			}
-			if($a[10] != 'NA') $buf .= "$a[1] bio2rdf:organism ".str_replace("taxid","taxon",$a[10]).".".PHP_EOL;
 		}
 		
-		// ref
+		// Participants
+		for($i=0;$i<2;$i++) { // the irefindex participant ids are at elements 0 and 1 of the array
+			$pid = $i+2; // the other ids for the irefindex are at elements 2 and 3 of the array
+			$other_ids = explode("|",$a[$pid]);
+			foreach($other_ids as $oid) {
+				BreakName($oid,$oid_ns,$oid_id,$oid_label);
+				if(isset($nsmap[$oid_ns])) $oid_ns = $nsmap[$oid_ns]; // check to see whether we're mapping the namespace
+				$buf .= "$a[$i] owl:sameAs $oid_ns:$oid_id.".PHP_EOL;
+			}
+			// organism
+			if($a[9] != 'NA') $buf .= "$a[$i] bio2rdf:organism ".str_replace("taxid","taxon",$a[9]).".".PHP_EOL;
+		}
+		// Participant types
+		BreakName($a[17], &$i1_type_ns, &$i1_type_id, &$i1_type_label);
+		BreakName($a[18], &$i2_type_ns, &$i2_type_id, &$i2_type_label);
+		$buf .= "$a[0] a $i1_type_ns:$i1_type_id .".PHP_EOL;
+		$buf .= "$a[1] a $i2_type_ns:$i2_type_id .".PHP_EOL;
+		$oids["$i1_type_ns:$i1_type_id"] = $i1_type_label;
+		$oids["$i2_type_ns:$i2_type_id"] = $i2_type_label;
+		
+
+		// relate the interaction to the participants, complex to the parts
+		$label = '';
+		$rel = $a[20];
+		$num = $a[21];
+		if($rel == 'C') {
+			// add the the part
+			$buf .= "$iid ss:hasProperPart $a[1].".PHP_EOL;	
+			$complexes [$iid][] = $a[1]; // keep this list to generate the labels at the end;		
+			$type = 'Complex';
+			$label = "$num component complex $m_label";
+		} else {
+			if($rel == 'Y') {
+				if(!$it_label) $it_label = 'Interaction';
+				if($num == 1) {
+					$type = 'SelfInteraction';
+					$label = "$m_label Interaction of $a[2] with itself";
+					$buf .= "$iid ss:hasAgent $a[0].".PHP_EOL;
+					// interacts with itself
+				} else {
+					$type = 'HomoInteraction';
+					$label = "$it_label between two molecules of $a[2] $m_label";
+					$buf .= "$iid ss:hasParticipant $a[0].".PHP_EOL;
+					// has exactly 2 of this type
+					/* @todo
+					<rdf:type>
+						<owl:Restriction>
+							<owl:onProperty rdf:resource="&ontology;hasPart"/>
+							<owl:onClass rdf:resource="&ontology;Protein"/>
+							<owl:qualifiedCardinality rdf:datatype="&xsd;nonNegativeInteger">2</owl:qualifiedCardinality>
+						</owl:Restriction>
+					</rdf:type>
+					*/					
+				}
+			} else if($rel == 'X') {
+				$type = 'HeteroInteraction';
+				$buf .= "$iid ss:hasParticipant $a[0].".PHP_EOL;
+				$buf .= "$iid ss:hasParticipant $a[1].".PHP_EOL;
+				$label = "$i1_type_label-$i2_type_label $it_label between $a[0] and $a[1] $m_label";
+			} else {
+				trigger_error("Unhandled type of interaction $rel");
+				exit;
+			}
+		}
+		$buf .= "$iid a ss:$type .".PHP_EOL;
+		$buf .= "$iid irefindex:number_of_interactors \"$num\".".PHP_EOL;
+		if($label) $buf .= "$iid rdfs:label \"$label [$iid]\".".PHP_EOL;
+	
+		
+		// article
 		$pubs = explode("|",$a[8]);
 		foreach($pubs AS $pub) {
 			$b = explode(":",$pub);
@@ -121,17 +151,28 @@ function irefindex($infp, $outfp)
 			$buf .= "$iid bio2rdf:article $b[0]:$b[1].".PHP_EOL;
 		}
 		
+		// provenance
 		$dbs = explode("|",$a[12]);
 		foreach($dbs AS $db) {
 			BreakName($db,$ns,$id,$label);
 			$buf .= "$iid bio2rdf:provenance $ns:$id.".PHP_EOL;
 			$oids["$ns:$id"] = $label;
 		}
-	
 		
+		// confidence
+		$confidences = explode("|",$a[14]);
+		foreach($confidences AS $c) {
+			// label:score
+			BreakName($c,&$label,&$score, &$nothing);
+			$buf .= "$iid irefindex:$label \"$score\".".PHP_EOL;
+		}
 		
-		//if($z++ == 6) {echo $buf;echo TriplesFromLabel($oids);exit;}
+		$liid = $iid;
 	}
+
+	//echo $buf;echo TriplesFromLabel($oids);exit;
+			
+	
 	fwrite($outfp,$buf);
 	fwrite($outfp,TriplesFromLabel($oids));
 	return 0;
@@ -141,7 +182,6 @@ function irefindex($infp, $outfp)
 // a name of the ns:id(label)
 function BreakName($name, &$ns, &$id, &$label)
 {
-	
 	preg_match("/(.*):(.*)/",$name,$matches);
 	$ns = strtolower($matches[1]);
 	$id = $matches[2];
