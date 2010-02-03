96a514ab8c1cec7f5b98e6f08fdab0b9289d244b
michel.dumontier <michel.dumontier@d60594c4-dda9-11dd-87d8-31aa04531ed5>
2010-02-03 23:46:38 +0000

    added links to presentations, mailing list and blog

--- trunk/bio2rdf/website/about.php
+++ trunk/bio2rdf/website/about.php
@@ -10,5 +10,17 @@
 <ul>
 <li>Francois Belleau, Marc-Alexandre Nolin, Nicole Tourigny, Philippe Rigault and Jean Morissette. <a href="http://dx.doi.org/10.1016/j.jbi.2008.03.004">Bio2RDF: Towards a mashup to build bioinformatics knowledge systems</a>. 2008. Journal of Biomedical Informatics 41:5 p706-716.</li>
 <li>Marc-Alexandre Nolin, Peter Ansell, Francois Belleau, Kingsley Idehen, Philippe Rigault, Nicole Tourigny, Paul Roe, James M Hogan, Michel Dumontier. <a href="http://dumontierlab.com/pdf/2008_ISWC_SWC_Bio2RDF.pdf">Bio2RDF Network Of Linked Data</a>. 2008. International Semantic Web Conference (ISWC2008): Semantic Web Challenge.</li>
+</ul>
+
+<h1>Presentations</h1>
+Bio2RDF presentations are published on <a title="Bio2RDF @ slidshare" href="http://www.slideshare.net/search/slideshow?q=bio2rdf">SlideShare</a>.
+
+<h1>Mailing List</h1>
+A Bio2RDF mailing list has been established using <a title="Bio2RDF mailing list on Google Group" href="http://groups.google.ca/group/bio2rdf">Google groups</a>.
+
+<h1>Blog</h1>
+The Bio2RDF blog is avilable through <a title="Bio2RDF Blog @ Blogspot" href="http://bio2rdf.blogspot.com/">Blogspot</a>.
+
+
 </div>
 <?php include('footer.php');?>
