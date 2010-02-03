85981e91aee2156949c969a80daa50626761cb69
michel.dumontier <michel.dumontier@d60594c4-dda9-11dd-87d8-31aa04531ed5>
2010-02-03 23:50:23 +0000

    minor

--- trunk/bio2rdf/website/index.php
+++ trunk/bio2rdf/website/index.php
@@ -10,33 +10,30 @@
  		<!-- Semantic knowledge discovery across the Life Sciences -->
 </head>
 <body>
-	<div id="content">
-	
-		<div class="logo">
-		 <img title="Bio2RDF logo" src="images/bio2rdf-logo.gif" width="100%"/>
-		 <span id="tagline">Linked Data for the Life Sciences</span>
-		</div>
- 
-		<div class="search">
-		 <form id="search-form">
-		  <input id="input" type="text" size="35" />
-		  <button id="search-button" type="submit" value="GO">GO</button>
-		 </form>
-		</div>
-	</div>
-	<div id="links">
-	 <div class="links">
-		[<a title="about" href="about.php">about</a>] 
-		[<a title="datasets" href="datasets.php">datasets</a>] 
-		[<a title="download" href="download.php">download</a>]
+ <div id="content">
+  <div class="logo">
+    <img title="Bio2RDF - Linked Data for the Life Sciences" src="images/bio2rdf-logo.gif" width="100%"/>
+    <span id="tagline">Linked Data for the Life Sciences</span>
+  </div>
+
+  <div class="search">
+   <form id="search-form">
+    <input id="input" type="text" size="35" />
+    <button id="search-button" type="submit" value="GO">GO</button>
+   </form>
+  </div>
+ </div>
+
+ <div id="links">
+  <div class="links">
+   [<a title="about" href="about.php">about</a>] 
+   [<a title="datasets" href="datasets.php">datasets</a>] 
+   [<a title="download" href="download.php">download</a>]
 		
-	<br><br><a title="Creative Commons - By Attribution - Share-Alike" rel="license" href="http://creativecommons.org/licenses/by-sa/2.5/ca/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/2.5/ca/80x15.png" /></a>
-	</div>
-	</div>
-	
-	<div id="footer">
-	
-	</div>
+   <br><br><a title="Creative Commons - By Attribution - Share-Alike" rel="license" href="http://creativecommons.org/licenses/by-sa/2.5/ca/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/2.5/ca/80x15.png" /></a>
+ </div>
+</div>
+<div id="footer"></div>
  
 </body>
 </html>
