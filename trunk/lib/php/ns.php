a59dbb5ac72f75a46f075c058b44116550796831
michel.dumontier <michel.dumontier@d60594c4-dda9-11dd-87d8-31aa04531ed5>
2010-02-15 21:12:12 +0000

    added a few more namespaces

--- trunk/lib/php/ns.php
+++ trunk/lib/php/ns.php
@@ -4,12 +4,16 @@ $xsd = 'xsd';         $xsd_uri  = 'http://www.w3.org/2001/XMLSchema#';
 $rdf = 'rdf';         $rdf_uri  = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#';
 $rdfs = 'rdfs';       $rdfs_uri = 'http://www.w3.org/2000/01/rdf-schema#';
 $owl = 'owl';         $owl_uri  = 'http://www.w3.org/2002/07/owl#';
-$dc = 'dc';           $dc_uri   = 'http://purl.org/dc/elements/1.1/';
+/* $dc = 'dc';           $dc_uri   = 'http://purl.org/dc/elements/1.1/'; */
+$dc = 'dc';           $dc_uri   = 'http://purl.org/dc/terms/';
+$skos = 'skos';       $skos_uri = 'http://www.w3.org/2004/02/skos/core#';
+$foaf = 'foaf';       $foaf_uri = 'http://xmlns.com/foaf/0.1/';
 $ss = 'ss';           $ss_uri   = 'http://semanticscience.org/resource/';
 $bio2rdfns = 'bio2rdf_ns';  $bio2rdfns_uri   = 'http://bio2rdf.org/ns/bio2rdf:';
 
 /** BIO2RDF **/
 $afcs = 'afcs';
+$apo = 'apo';
 $bio2rdf = 'bio2rdf';
 $bm = $bio2rdf_uri = 'http://bio2rdf.org/';
 $bind = 'bind';
@@ -18,14 +22,16 @@ $candida = 'candida';
 $cas = 'cas'; 
 $chebi = 'chebi';
 $ctd = 'ctd';
+$dbsnp = 'dbsnp';
 $dip = 'dip';
 $ddbj = 'ddbj';
+$drugbank = 'drugbank';
 $ec  = 'ec'; 
 $embl = 'embl';
 $ensembl = 'ensembl';
 $eco = 'eco';
 $euroscarf = 'euroscarf'; 
-$flybase = 'fb';
+$flybase = 'flybase';
 $kegg = 'kegg';
 $germonline = 'germonline'; 
 $go = 'go';
@@ -33,6 +39,7 @@ $gp = 'gp'; // ncbi genome projects
 $grid = 'grid';
 $iubmb = 'iubmb';
 $intact = 'intact';
+$ipi = 'ipi';
 $irefindex = 'irefindex';
 $mesh = 'mesh'; 
 $metacyc = 'metacyc';
@@ -53,6 +60,7 @@ $pdb    = 'pdb';
 $pubmed = 'pubmed';
 $pubchem = 'pubchem';
 $sgd    = 'sgd';
+$so = 'so';
 $sp     = 'swissprot';
 $taxon  = 'taxon';
 $tcdb = 'tcdb';
@@ -61,18 +69,20 @@ $tpg = 'tpg';
 $trembl = 'trembl';
 $uniparc = 'uniparc';
 $uniprot = 'uniprot';
+$uo = 'uo';
+$serv = 'serv';
 
-$basenslist = array($xsd => $xsd_uri, $rdf => $rdf_uri, $rdfs => $rdfs_uri, $owl => $owl_uri, $dc => $dc_uri, $ss =>$ss_uri);
+$basenslist = array($xsd => $xsd_uri, $rdf => $rdf_uri, $rdfs => $rdfs_uri, $owl => $owl_uri, $dc => $dc_uri, $skos => $skos_uri,$foaf => $foaf_uri, $ss =>$ss_uri);
 
 $nslist = array(
-	$xsd => $xsd_uri, $rdf => $rdf_uri, $rdfs => $rdfs_uri, $owl => $owl_uri, $dc => $dc_uri, $ss =>$ss_uri, 
-	$afcs, $bind,$biogrid, $cas, $candida, $ctd, $ddbj,$dip, $ec, $embl, $ensembl, $eco, 
+	$xsd => $xsd_uri, $rdf => $rdf_uri, $rdfs => $rdfs_uri, $owl => $owl_uri, $dc => $dc_uri, $skos => $skos_uri, $foaf=>$foaf_uri, $ss =>$ss_uri,  
+	$afcs, $apo, $bind,$biogrid, $cas, $candida, $ctd,$dbsnp, $ddbj,$dip,$drugbank, $ec, $embl, $ensembl, $eco, 
 	$euroscarf, $flybase, $kegg, $germonline, $go, $gp, $grid, 
-	$iubmb, $intact,$irefindex,  $mesh, $metacyc,$mi,$mint,$mips,
+	$ipi, $iubmb, $intact,$irefindex,  $mesh, $metacyc,$mi,$mint,$mips,
 	$ncbi_gene, $refseq, $ncbi, $omim, $ophid, $pdb,$pharmgkb, $pir,$prf, $pubchem, $pubmed, 
-	$sgd, $sp, $taxon, $tcdb, $tigr, $tpg, $trembl, $uniparc, $uniprot,
+	$so, $sgd, $sp, $taxon, $tcdb, $tigr, $tpg, $trembl, $uniparc, $uniprot,
 	"profilescan","superfamily","patternscan","blastprodom","fprintscan","gene3d","seg","hmmsmart","hmmpanther","hmmpfam","hmmpir","hmmtigr",
-	$bio2rdf => $bio2rdf_uri, $bio2rdfns => $bio2rdfns_uri 
+	$bio2rdf => $bio2rdf_uri, $bio2rdfns => $bio2rdfns_uri, $serv, $uo 
 );
 
 
