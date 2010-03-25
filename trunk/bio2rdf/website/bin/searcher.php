5c8e795f2ea32a0b8e4261b693bf60210267ba50
michel.dumontier <michel.dumontier@d60594c4-dda9-11dd-87d8-31aa04531ed5>
2010-03-25 01:24:49 +0000

    update

--- trunk/bio2rdf/website/bin/searcher.php
+++ trunk/bio2rdf/website/bin/searcher.php
@@ -1,10 +1,10 @@
 <?php
 $str = '<p id="searchresults">';
-$str .= '<span class="category">category one</span>';
-$str .= '<a href="http://google.ca">
-	<span class="searchheading">Result Label</span>
-	<span>Lorem ipsum dolor sit am velit ac sem pulvinar tempor sed egestas augue. Donec imperdiet accumsan elementum
-.</span>
+$str .= '<span class="category">Data</span>';
+$str .= '<a href="http://bio2rdf.org/geneid:3098">
+ <span class="searchheading">Hexokinase [geneid:3098]</span>
+ <span class="searchdef">Hexokinases phosphorylate glucose to produce glucose-6-phosphate, the first step in most glucose metabolism pathways...</span>
+ <span class="search_dataset">An entity described in the Entrez Gene dataset published by the NCBI.</span>
 </a>';
 $str .= '<a href="http://cbc.ca">
         <span class="searchheading">Result Label</span>
@@ -16,7 +16,7 @@ $str .= '<a href="http://cnn.com">
         <span>Lorem ipsum dolor sit amellentesque id velit ac sem pulvinar tempor sed egestas augue. Donec imperdiet accu
 msan elementum.</span>
 </a>';
-$str .= '<span class="category">category two</span>';
+$str .= '<span class="category">Dataset</span>';
 $str .= '<a href="http://google.ca">
 	<span class="searchheading">Result Label</span>
 	<span>Lorem ipsum dolor sit am velit ac sem pulvinar tempor sed egestas augue. Donec imperdiet accumsan elementum
