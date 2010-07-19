e38bac44186de13f0ca604f5473665ebc3fb335c
alison.callahan <alison.callahan@d60594c4-dda9-11dd-87d8-31aa04531ed5>
2010-07-19 17:41:57 +0000

    testing 1234

--- branches/alison/bio2rdf/website/footer.php
+++ branches/alison/bio2rdf/website/footer.php
@@ -1,6 +1,6 @@
         </div>
         <div id="footer">
-                <p><a title="Test Creative Commons - By Attribution - Share-Alike" rel="license" href="http://creativecommons.org/licenses/by-sa/2"></a>
+                <p><a title="Creative Commons - By Attribution - Share-Alike" rel="license" href="http://creativecommons.org/licenses/by-sa/2"></a>
         </div>
 </body>
 </html>        
