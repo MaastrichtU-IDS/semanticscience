5963659ab2090cbe6aa148d3d174cad5993c3151
josemiguelcruztoledo@gmail.com <josemiguelcruztoledo@gmail.com@d60594c4-dda9-11dd-87d8-31aa04531ed5>
2010-09-17 17:26:08 +0000

    first commit

---
+++ branches/jose/java/aptamer-base-connector/src/main/java/com/dumontierlab/aptamer_base_connector/App.java
@@ -0,0 +1,13 @@
+package com.dumontierlab.aptamer_base_connector;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
