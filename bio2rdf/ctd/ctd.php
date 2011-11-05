48214acd779dd97795c6fd8463773087a88080b3
Michel Dumontier <michel.dumontier@gmail.com>
2011-11-05 23:34:11 +0000

    syncing

--- bio2rdf/ctd/ctd.php
+++ bio2rdf/ctd/ctd.php
@@ -24,13 +24,15 @@ $infile_suffix = ".tsv.gz";
 $outfile_suffix = ".n3.gz";
 $allfiles = array(
  "chemicals",  // uses MESH terms
- "chem_gene_ixns", // curated interactions
  "chem_disease_relations",
+// "chem_gene_ixn_types", // curated interactions
+ "chem_gene_ixns", // curated interactions
  "chem_pathways",  // 
  "diseases",   // uses MESH/OMIM
  "disease_pathways",
  "gene_disease_relations", 
- "gene_pathways"
+ "gene_pathways",
+// "genes"
 );
 
 if($options['dataset'][0] == '*') {
@@ -62,9 +64,11 @@ foreach($files AS $base) {
 	
 	
 	$fnx = "CTD_".$base;
-	if($fnx($infp,$outfp)) {
-		trigger_error("Error in $fnx");
-		exit;
+	if(isset($fnx)) {
+		if($fnx($infp,$outfp)) {
+		 	trigger_error("Error in $fnx");
+			 exit;
+                }
 	}
 	echo "Converted to $outfile\n";
 	
@@ -77,27 +81,35 @@ foreach($files AS $base) {
 
 
 /*
-x ChemicalName
-X CasRN
-X ChemicalID (MeSH accession identifier)
+Fields:
+0 ChemicalName
+1 ChemicalID (MeSH accession identifier)
+2 CasRN
+3 ParentIDs (accession identifiers of the parent terms; '|'-delimited list)
+4 ChemicalTreeNumbers (unique identifiers of the chemical's nodes; '|'-delimited list)
+5 ParentTreeNumbers (unique identifiers of the parent nodes; '|'-delimited list)
+6 Synonyms ('|'-delimited list)
 */
 function CTD_chemicals($infp, $outfp)
 {
 	require (dirname(__FILE__).'/../include.php');
 	$buf = N3NSHeader($nslist);
-	
+
 	gzgets($infp);
 	while($l = gzgets($infp)) {
-		list($name,$casrn,$mid) = explode("\t",trim($l));
+		list($name,$mid,$casrn) = explode("\t",trim($l));
+		$m=explode(":",$mid);
+		$mid = $m[1];
 		$name = addslashes($name);
+
 		$buf .= "$mesh:$mid $dc:identifier \"$mesh:$mid\" .".PHP_EOL;
 		$buf .= "$mesh:$mid $dc:title \"$name\" .".PHP_EOL;
 		$buf .= "$mesh:$mid $rdfs:label \"$name [$mesh:$mid]\" .".PHP_EOL;  // [ns:id] title ?
 		
-		$buf .= "$mesh:$mid $rdfs:subClassOf $bio2rdfns:Chemical .".PHP_EOL;
-		if($casrn) $buf .= "$mesh:$mid $rdfs:seeAlso $cas:$casrn .".PHP_EOL;
+		$buf .= "$mesh:$mid a $ctd_resource:Chemical .".PHP_EOL;
+		if($casrn) $buf .= "$mesh:$mid $owl:equivalentClass $cas:$casrn .".PHP_EOL;
 		
-		$buf .= "$mesh:$mid $bio2rdfns:in $ss:dataset/$ctd .".PHP_EOL;
+		$buf .= "$mesh:$mid $ctd_resource:in $ss:dataset/$ctd .".PHP_EOL;
 	}
 	gzwrite($outfp,$buf);
 	return 0;
@@ -105,47 +117,50 @@ function CTD_chemicals($infp, $outfp)
 
 
 /* CURATED chemical-gene interactions
-  ChemicalName
-X ChemicalID (MeSH accession identifier)
-  CasRN
-  GeneSymbol
-X GeneID (NCBI Gene or CTD accession identifier)
-  Organism (scientific name)
-X OrganismID (NCBI Taxonomy accession identifier)
-x Interaction
-X PubmedIDs ('|'-delimited list)
-*/
-function CTD_chem_gene_ixns($infp, $outfp)
-{
+  0 ChemicalName
+X 1 ChemicalID (MeSH accession identifier)
+  2 CasRN
+  3 GeneSymbol
+X 4 GeneID (NCBI Gene or CTD accession identifier)
+  5 Organism (scientific name)
+X 6 OrganismID (NCBI Taxonomy accession identifier)
+x 7 Interaction
+  8 InteractionActions ('|'-delimited list)
+X 9 PubmedIDs ('|'-delimited list) */ 
+
+function CTD_chem_gene_ixns($infp, $outfp) {
 	require (dirname(__FILE__).'/../include.php');
 	$buf = N3NSHeader($nslist);
 	
 	gzgets($infp);
 	while($l = gzgets($infp)) {
 		$a = explode("\t",trim($l));
+
 		$mid = $a[1];
 		$gene_id = $a[4];
 		$taxon_id = $a[6];
 		$interaction_text = $a[7];
-		$pubmed_ids = explode("|",$a[8]);
+		$interaction_action = $a[8];
+		$pubmed_ids = explode("|",$a[9]);
 		foreach($pubmed_ids AS $i=> $pmid) {
 			if(!is_int($pmid)) unset($pubmed_ids[$i]);
 		}
 		
-		$uri = "$ctd:$mid$gene_id";  // should taxon be part of the ID?
+		$uri  = "$ctd_resource:$mid$gene_id";  // should taxon be part of the ID?
 		
-//		$buf .= "$uri $dc:identifier \"$ctd:$mid$gene_id\".".PHP_EOL;
-		$buf .= "$uri $rdfs:label \"interaction between gene $gene_id and chemical $mid [$ctd:$mid$gene_id]\".".PHP_EOL;
-		$buf .= "$uri $rdf:type $ctd:ChemicalGeneInteraction.".PHP_EOL;
+		$buf .= "$uri $dc:identifier \"ctd_resource:$mid$gene_id\".".PHP_EOL;
+		$buf .= "$uri $rdfs:label \"interaction between $a[3] (geneid:$gene_id) and $a[0] (mesh:$mid) [$ctd:$mid$gene_id]\".".PHP_EOL;
+		$buf .= "$uri $rdf:type $ctd_resource:ChemicalGeneInteraction.".PHP_EOL;
 		
 		$buf .= "$uri $rdfs:comment \"$a[7]\".".PHP_EOL;
-		$buf .= "$uri $bio2rdfns:gene $ncbi_gene:$gene_id .".PHP_EOL;
-		$buf .= "$uri $bio2rdfns:chemical $mesh:$mid .".PHP_EOL;
-		if($taxon_id) $buf .= "$uri $bio2rdfns:organism $taxon:$taxon_id .".PHP_EOL;
-		if($pubmed_ids) foreach($pubmed_ids AS $pubmed_id) $buf .= "$uri $bio2rdfns:article $pubmed:$pubmed_id .".PHP_EOL;
+		$buf .= "$uri $ctd_resource:gene $ncbi_gene:$gene_id .".PHP_EOL;
+		$buf .= "$uri $ctd_resource:chemical $mesh:$mid .".PHP_EOL;
+		if($taxon_id) $buf .= "$uri $ctd_resource:organism $taxon:$taxon_id .".PHP_EOL;
+		if($pubmed_ids) foreach($pubmed_ids AS $pubmed_id) $buf .= "$uri $ctd_resource:article $pubmed:$pubmed_id .".PHP_EOL;
+		if($interaction_action) $buf .= "$uri $ctd_resource:action \"$interaction_action\" .".PHP_EOL;
 		//break;
 	
-		//echo $buf;exit;
+//		echo $buf;exit;
 	}
 	gzwrite($outfp,$buf);
 	return 0;
@@ -154,42 +169,51 @@ function CTD_chem_gene_ixns($infp, $outfp)
 
 
 /*
-  ChemicalName
-X ChemicalID (MeSH accession identifier)
-  CasRN
-  DiseaseName
-X DiseaseID (MeSH or OMIM accession identifier)
-x ChemicalDiseaseRelation
-X OmimIDs ('|'-delimited list)
-X PubmedIDs ('|'-delimited list)
+X 0 ChemicalName
+X 1 ChemicalID (MeSH accession identifier)
+  2 CasRN
+X 3 DiseaseName
+X 4 DiseaseID (MeSH or OMIM accession identifier)
+  5 DirectEvidence
+  6 InferenceGeneSymbol
+x 7 InferenceScore
+X 8 OmimIDs ('|'-delimited list)
+X 9 PubmedIDs ('|'-delimited list)
 */
 function CTD_chem_disease_relations($infp, $outfp)
 {
 	require (dirname(__FILE__).'/../include.php');
 	$buf = N3NSHeader($nslist);
-	
+
 	gzgets($infp);
 	while($l = gzgets($infp)) {
 		$a = explode("\t",trim($l));
-		$cid = $a[1];
-
-		$mesh_id = substr($a[4],strpos($a[4], ":")+1); // MESH:D000544
-		if($a[6]) $omim_ids = explode("|",strtolower($a[6]));
-		$pubmed_ids = explode("|",$a[7]);
+		$chemical_name = $a[0];
+		$chemical_id = $a[1];
+		$disease_name = $a[3];
+		$disease = explode(":",$a[4]);
+		$disease_ns = strtolower($disease[0]);
+		$disease_id = $disease[1];
+				
+		$uid = "$chemical_id$disease_id";
+		$uri = "$ctd_resource:$uid";
 		
-		$uri = "$ctd:$cid$mesh_id";
-		
-		//$buf .= "$uri $dc:identifier \"$uri\".".PHP_EOL;
-		$buf .= "$uri $rdfs:label \"interaction between chemical $cid and disease $mesh_id [$uri]\".".PHP_EOL;
-		$buf .= "$uri a $ctd:ChemicalDiseaseInteraction .".PHP_EOL;
-		$buf .= "$uri $bio2rdfns:chemical $mesh:$cid .".PHP_EOL;
-		$buf .= "$uri $bio2rdfns:disease $mesh:$mesh_id .".PHP_EOL;
-		if($omim_id)    foreach($omim_ids AS $omim_id)     $buf .= "$uri $bio2rdfns:disease $omim:$omim_id .".PHP_EOL;
-		if($pubmed_ids) foreach($pubmed_ids AS $pubmed_id) {
-			if($pubmed_id) $buf .= "$uri $bio2rdfns:article $pubmed:$pubmed_id .".PHP_EOL;
+		$buf .= "$uri $dc:identifier \"ctd_resource:$uid\".".PHP_EOL;
+		$buf .= "$uri $rdfs:label \"interaction between $chemical_name ($chemical_id) and disease $disease_name ($disease_ns:$disease_id) [$uri]\".".PHP_EOL;
+		$buf .= "$uri a $ctd_resource:ChemicalDiseaseInteraction .".PHP_EOL;
+		$buf .= "$uri $ctd_resource:chemical $mesh:$chemical_id .".PHP_EOL;
+		$buf .= "$uri $ctd_resource:disease $disease_ns:$disease_id .".PHP_EOL;
+		if($a[8])  {
+			$omim_ids = explode("|",strtolower($a[8]));
+			foreach($omim_ids AS $omim_id)     $buf .= "$uri $ctd_resource:disease $omim:$omim_id .".PHP_EOL;
+		}
+		if(isset($a[9])) {
+			$pubmed_ids = explode("|",$a[9]);
+			foreach($pubmed_ids AS $pubmed_id) {
+				if($pubmed_id) $buf .= "$uri $ctd_resource:article $pubmed:$pubmed_id .".PHP_EOL;
+			}
 		}
 		
-		//echo $buf;exit;
 	}
 	
 	gzwrite($outfp,$buf);
@@ -197,12 +221,12 @@ function CTD_chem_disease_relations($infp, $outfp)
 }
 
 /*
-  ChemicalName
-X ChemicalID (MeSH accession identifier)
-  CasRN
-  PathwayName
-X PathwayID (KEGG accession identifier)
-x ChemicalPathwayRelation
+  0 ChemicalName
+X 1 ChemicalID (MeSH accession identifier)
+  2 CasRN
+  3 PathwayName
+X 4 PathwayID (KEGG accession identifier)
+x 5 InferenceGeneSymbol
 */
 function CTD_chem_pathways($infp, $outfp)
 {
@@ -212,21 +236,11 @@ function CTD_chem_pathways($infp, $outfp)
 	gzgets($infp);
 	while($l = gzgets($infp)) {
 		$a = explode("\t",trim($l));
-		$mid = $a[1];
-		$pathway_id = substr($a[4],strpos($a[4], ":")+1); // MESH:D000544
+		$chemical_id = $a[1];
+		ParseQNAME($a[4],$pathway_ns,$pathway_id);
+		if($pathway_ns == "react") $pathway_ns = "reactome";
 		
-		$buf .= "$mesh:$mid $bio2rdfns:pathway $kegg:$pathway_id .".PHP_EOL;
-		
-		// @TODO NEED axiom annotation
-		$uri = "$ctd:$mid$pathway_id";
-		$buf .= "$uri a $owl:Axiom .".PHP_EOL;
-		// $buf .= "$uri $dc:identifier \"$uri\".".PHP_EOL;
-		$buf .= "$uri $rdfs:label \"$a[5] [$uri]\".".PHP_EOL;
-		$buf .= "$uri $owl:subject $mesh:$mid.".PHP_EOL;
-		$buf .= "$uri $owl:predicate $bio2rdfns:pathway.".PHP_EOL;
-		$buf .= "$uri $owl:object $kegg:$pathway_id.".PHP_EOL;
-	
-		//break;
+		$buf .= "$mesh:$chemical_id $ctd_resource:pathway $pathway_ns:$pathway_id .".PHP_EOL;		
 	}
 	
 	gzwrite($outfp,$buf);
@@ -234,9 +248,13 @@ function CTD_chem_pathways($infp, $outfp)
 }
 
 /*
-  DiseaseName
-X DiseaseDB (MeSH or OMIM)
-X DiseaseID (MeSH or OMIM accession identifier)
+  0 DiseaseName
+X 1 DiseaseID (MeSH or OMIM accession identifier)
+  2 AltDiseaseIDs
+  3 ParentIDs
+  4 DiseaseTreeNumbers
+  5 ParentTreeNumbers
+  6 Synonyms
 */
 function CTD_diseases($infp, $outfp)
 {
@@ -244,12 +262,14 @@ function CTD_diseases($infp, $outfp)
 	$buf = N3NSHeader($nslist);
 	
 	gzgets($infp);
+	gzgets($infp);
 	while($l = gzgets($infp)) {
 		$a = explode("\t",trim($l));
-		$id = strtolower($a[1]).':'.$a[2];
+		ParseQNAME($a[1],$disease_ns,$disease_id);
 
-		$buf .= "$id $rdfs:label \"$a[0] [$id]\" .".PHP_EOL;
-		$buf .= "$id $rdfs:subClassOf $ctd:Disease .".PHP_EOL;
+		$uid = "$disease_ns:$disease_id";
+		$buf .= "$uid $rdfs:label \"$a[0] [$uid]\" .".PHP_EOL;
+		$buf .= "$uid a $ctd_resource:Disease .".PHP_EOL;
 		
 		//echo $buf;exit;
 	}
@@ -264,6 +284,7 @@ function CTD_diseases($infp, $outfp)
 X DiseaseID (MeSH or OMIM accession identifier)
   PathwayName
 X PathwayID (KEGG accession identifier)
+  InferenceGeneSymbol
 */
 function CTD_disease_pathways($infp, $outfp)
 {
@@ -273,17 +294,17 @@ function CTD_disease_pathways($infp, $outfp)
 	gzgets($infp);
 	while($l = gzgets($infp)) {
 		$a = explode("\t",trim($l));
-		$id = explode(":",$a[1]);
-		$disease_id = strtolower($id[0]).':'.$id[1];
-		$pathway_id = strtolower($a[3]); // includes kegg: prefix
+		ParseQNAME($a[1],$disease_ns,$disease_id);
+		ParseQNAME($a[3],$pathway_ns,$pathway_id);
+		if($pathway_ns == 'react') $pathway_ns = 'reactome';
 
-		$buf .= "$disease_id $bio2rdfns:pathway $pathway_id .".PHP_EOL;
+		$buf .= "$disease_ns:$disease_id $ctd_resource:pathway $pathway_ns:$pathway_id .".PHP_EOL;
 		
 		// extra
-		$buf .= "$disease_id $dc:identifer \"$disease_id\" .".PHP_EOL;
-		$buf .= "$disease_id $rdfs:label \"$a[0] [$disease_id]\" .".PHP_EOL;
-		$buf .= "$pathway_id $dc:identifer \"$pathway_id\" .".PHP_EOL;
-		$buf .= "$pathway_id $rdfs:label \"$a[2] [$pathway_id]\" .".PHP_EOL;
+		$buf .= "$disease_ns:$disease_id $dc:identifer \"$disease_ns:$disease_id\" .".PHP_EOL;
+		$buf .= "$disease_ns:$disease_id $rdfs:label \"$a[0] [$disease_ns:$disease_id]\" .".PHP_EOL;
+		$buf .= "$pathway_ns:$pathway_id $dc:identifer \"$pathway_ns:$pathway_id\" .".PHP_EOL;
+		$buf .= "$pathway_ns:$pathway_id $rdfs:label \"$a[2] [$pathway_ns:$pathway_id]\" .".PHP_EOL;
 	}
 	
 	gzwrite($outfp,$buf);
@@ -292,13 +313,15 @@ function CTD_disease_pathways($infp, $outfp)
 
 
 /*
-  GeneSymbol
-X GeneID (NCBI Gene or CTD accession identifier)
-  DiseaseName
-X DiseaseID (MeSH or OMIM accession identifier)
-x GeneDiseaseRelation
-X OmimIDs ('|'-delimited list)
-X PubmedIDs ('|'-delimited list)
+  0 GeneSymbol
+X 1 GeneID (NCBI Gene or CTD accession identifier)
+  2 DiseaseName
+X 3 DiseaseID (MeSH or OMIM accession identifier)
+  4 DirectEvidence
+  5 InferenceChemicalName
+  6 InferenceScore
+X 7 OmimIDs ('|'-delimited list)
+X 8 PubmedIDs ('|'-delimited list)
 */
 function CTD_gene_disease_relations($infp, $outfp)
 {
@@ -308,29 +331,29 @@ function CTD_gene_disease_relations($infp, $outfp)
 	gzgets($infp);
 	while($l = gzgets($infp)) {
 		$a = explode("\t",trim($l));
-		$gene_id    = $a[1];
-		$disease_id = explode(":",$a[3]);
-		unset($mesh_id);unset ($omim_id);
+		$gene_name = $a[0];
+		$gene_ns = 'geneid';
+		$gene_id = $a[1];
+		$disease_name = $a[2];
+		ParseQNAME($a[3],$disease_ns,$disease_id);
 		
-		if(strstr($disease_id[0],"MESH")) $mesh_id = $disease_id[1];
-		else if(strstr($disease_id[0],"OMIM")) $omim_id = $disease_id[1];
-		if($a[5]) {
-			$omim_ids = explode("|",$a[5]);
-		}
-		if(isset($omim_id) && $omim_ids) $omim_ids[] = $omim_id;
-
-		$pubmed_ids = explode("|",$a[6]);
+		$uri = "$ctd_resource:$gene_id$disease_id";
 		
-		$uri = "$ctd:$gene_id$disease_id[1]";
-		$buf .= "$uri $rdf:type $ctd:GeneDiseaseInteraction .".PHP_EOL;
+		$buf .= "$uri $rdf:type $ctd_resource:GeneDiseaseInteraction .".PHP_EOL;
 		$buf .= "$uri $dc:identifier \"$uri\".".PHP_EOL;
-		$buf .= "$uri $rdfs:label \"Gene Disease interaction between gene $gene_id and disease $disease_id[0]:$disease_id[1] [$uri]\".".PHP_EOL;
-		$buf .= "$uri $bio2rdfns:gene $gene_id .".PHP_EOL;
-		if($mesh_id) $buf .= "$uri $bio2rdfns:disease $mesh:$mesh_id .".PHP_EOL;
-		if($omim_ids)   foreach($omim_ids AS $omim_id)    $buf .= "$uri $bio2rdfns:disease $omim:$omim_id .".PHP_EOL;
-		if($pubmed_ids) foreach($pubmed_ids AS $pubmed_id) {
-			if(!is_numeric($pubmed_id)) continue;
-			$buf .= "$uri $bio2rdfns:article $pubmed:$pubmed_id .".PHP_EOL;
+		$buf .= "$uri $rdfs:label \"Gene Disease interaction between $gene_name ($gene_ns:$gene_id) and $disease_name ($disease_ns:$disease_id) [$uri]\".".PHP_EOL;
+		$buf .= "$uri $ctd_resource:gene $gene_ns:$gene_id .".PHP_EOL;
+		$buf .= "$uri $ctd_resource:disease $disease_ns:$disease_id .".PHP_EOL;
+		if($a[7]) {
+			$omim_ids = explode("|",$a[7]);			
+			foreach($omim_ids AS $omim_id)    $buf .= "$uri $ctd_resource:disease $omim:$omim_id .".PHP_EOL;
+		}
+		if(isset($a[8])) {
+			$pubmed_ids = explode("|",$a[8]);
+			foreach($pubmed_ids AS $pubmed_id) {
+				if(!is_numeric($pubmed_id)) continue;
+				$buf .= "$uri $ctd_resource:article $pubmed:$pubmed_id .".PHP_EOL;
+			}
 		}
 		
 		//echo $buf; exit;
@@ -341,10 +364,10 @@ function CTD_gene_disease_relations($infp, $outfp)
 }
 
 /*
-  GeneSymbol
-X GeneID (NCBI Gene or CTD accession identifier)
-x PathwayName
-X PathwayID (KEGG accession identifier)
+  0 GeneSymbol
+X 1 GeneID (NCBI Gene or CTD accession identifier)
+x 2 PathwayName
+X 3 PathwayID (KEGG accession identifier)
 */
 function CTD_gene_pathways($infp, $outfp)
 {
@@ -354,21 +377,38 @@ function CTD_gene_pathways($infp, $outfp)
 	gzgets($infp);
 	while($l = gzgets($infp)) {
 		$a = explode("\t",trim($l));
+		$gene_ns = $ncbi_gene;
 		$gene_id = $a[1];
+		ParseQNAME($a[3],$pathway_ns,$pathway_id);
 		$kegg_id = strtolower($a[3]);
+		if($pathway_ns == "react") $pathway_ns = "reactome";
 
-		$buf .= "$ncbi_gene:$gene_id $bio2rdfns:pathway $kegg_id .".PHP_EOL;
+		$buf .= "$gene_ns:$gene_id $ctd_resource:pathway $pathway_ns:$pathway_id .".PHP_EOL;
 		
 		// extra
-		$buf .= "$kegg_id $dc:identifer \"$kegg_id\" .".PHP_EOL;
-		$buf .= "$kegg_id $rdfs:label \"$a[2] [$kegg_id]\" .".PHP_EOL;
-		$buf .= "$ncbi_gene:$gene_id $dc:identifer \"$ncbi_gene:$gene_id\" .".PHP_EOL;
-		$buf .= "$ncbi_gene:$gene_id $rdfs:label \"gene ".addslashes($a[0])." [$ncbi_gene:$gene_id]\" .".PHP_EOL;
+		$buf .= "$pathway_ns:$pathway_id $dc:identifer \"$pathway_ns:$pathway_id\" .".PHP_EOL;
+		$buf .= "$pathway_ns:$pathway_id $rdfs:label \"$a[2] [$pathway_ns:$pathway_id]\" .".PHP_EOL;
+		$buf .= "$gene_ns:$gene_id $dc:identifer \"$gene_ns:$gene_id\" .".PHP_EOL;
+		$buf .= "$gene_ns:$gene_id $rdfs:label \"gene ".addslashes($a[0])." [$gene_ns:$gene_id]\" .".PHP_EOL;
+
+//echo $buf;exit;
 	}
 	
 	gzwrite($outfp,$buf);
 	return 0;
 }
 
+
+function ParseQNAME($string,&$ns,&$id)
+{
+	$a = explode(":",$string);
+	if(count($a) == 1) {
+		$id = $string;
+	} else {
+		$ns = strtolower($a[0]);
+		$id = $a[1];
+	}
+	return true;
+}
 
 ?>
