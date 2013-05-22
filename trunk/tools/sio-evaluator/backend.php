44e88b0157c6b57b8320307b08b44b99a0c35dc7
michel.dumontier@gmail.com <michel.dumontier@gmail.com@d60594c4-dda9-11dd-87d8-31aa04531ed5>
2013-05-22 19:41:21 +0000

    initial import of stub

---
+++ trunk/tools/sio-evaluator/backend.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * class;
+ * functions 
+ *  get next term (user id) {returns termid}
+ *  get annotation (term id) {return obj->{id,termid,type,string}}
+ *  get subclass axiom (term id)  {return obj->{id,termid,type,string} }
+ *  store result ($result->{id,termid,yes|no|idk,comment}}
+*/
---
+++ trunk/tools/sio-evaluator/frontend.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * ask for an axiom (annotation or subclass axiom) in which the IP hasn't answered, and needs answers [1<5] or random
+ * present a question: do you agree that [term] [has definition|is a type of] [annotation|class expression]
+ * present radio box / 3 buttons : yes, no, i don't know
+ * present a comment box
+ * store the result with IP address
+*/
