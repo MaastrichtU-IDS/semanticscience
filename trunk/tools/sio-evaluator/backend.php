4bfc63ae2f7abc15d826962a9086b96e84056a0e
josemiguelcruztoledo@gmail.com <josemiguelcruztoledo@gmail.com@d60594c4-dda9-11dd-87d8-31aa04531ed5>
2013-05-23 18:58:24 +0000

    added class retriever

--- trunk/tools/sio-evaluator/backend.php
+++ trunk/tools/sio-evaluator/backend.php
@@ -1,25 +1,26 @@
 <?php
 /**
-Copyright (C) 2012 Michel Dumontier and Jose Cruz-Toledo
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-of the Software, and to permit persons to whom the Software is furnished to do
-so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+* Copyright (C) 2012 Michel Dumontier and Jose Cruz-Toledo
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy of
+* this software and associated documentation files (the "Software"), to deal in
+* the Software without restriction, including without limitation the rights to
+* use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+* of the Software, and to permit persons to whom the Software is furnished to do
+* so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
 */
+require_once("lib/connector.php");
 
 /**
  * class;
@@ -31,55 +32,32 @@ SOFTWARE.
 */
 
 class SEBackend{
-	/**
-	* Database credentials
-	*/
-	private $host = "134.117.108.159";
-	private $user = "sio-evaluator";
-	private $pass = "sio-evaluator-secret";
-	private $db = "sio-evaluator";
+	//a connection to the db object
 	private $conn = null;
+	private $sio_location = "/home/jose/Documents/ontologies/sio/sio.owl";
 
 	public function __construct(){
 		//create a connection to the database 
-		$conn = $this->createConnection(
-			$this->getHost(), 
-			$this->getUser(), 
-			$this->getPass(), 
-			$this->getDatabase()
-		);
+		$this->conn = new Connector();
 		//check if the DB's tables have been created and populated
 		if($this->isDbPopulated() == false){
 			// empty the database and create tables from scratch
+			$classes = $this->retrieveSIOClasses();
+			print_r($classes);
 
 		}
 	}
 
-	/** 
-	* Create a connection to the sio-evaluator database.
-	* If cannot create one, program fails and exits
-	*/
-	private function createConnection($h, $u, $p, $db){
-		$rm = mysqli_connect($h, $u, $p, $db);
-		//check conn
-		if(mysqli_connect_errno($rm)){
-			echo "Failed to connect to Database: ". mysqli_connect_error();
-			exit;
-		}
-		return $rm;
-	}
+	
 
-	private function getDirectParentOf($aQname){
+	private function getClassAxiomsOf($aQname){
 
 	}
 
-	private function getAnnotaitonFor($aQname){
+	private function getAnnotaitonsFor($aQname){
 
 	}
 
-	private function getDescendantsOf($aQname){
-		
-	}
 
 	/**
 	* Check if the database for this sio-evaluator has been populated.
@@ -87,6 +65,7 @@ class SEBackend{
 	* false otherwise
 	*/
 	private function isDbPopulated(){
+
 		//first check if database exists
 		return false;
 	}
@@ -115,6 +94,42 @@ class SEBackend{
 		return $this->conn;
 	}
 
+
+	//populate qname2annotation
+	private function populateQname2Annotation(){
+		//get the list of classes in sio
+	}
+
+	/**
+	* Create and return an array of SIO class URIs
+	*/
+	private function retrieveSIOClasses(){
+		
+		//owltools /home/jose/owl/sio.owl --list-classes
+		$r = shell_exec("owltools ".$this->sio_location." --list-classes") or die( "Could not run owltools!");
+		$rm = array();
+		$lines = explode("\n", $r);
+		foreach ($lines as $aLine) {
+			preg_match("/^<(http:\/\/.*)>$/", $aLine, $matches);
+			if(count($matches)){
+				$aUri = trim($matches[1]);
+				$rm[] = $aUri;
+			}
+		}
+		return $rm;
+	}
+
+	/**
+	* return the dc:description for the given URI
+	* @param $someClassUri a of a SIO class 
+	*/
+	private function retriveClassAnnotation($someClassUri){
+		$rm = array();
+		foreach ($someClassUris as $aUri) { 
+			
+		}
+		return $rm;
+	}
 
 }//class
 
