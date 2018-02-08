cc64a41e00f8daea1b594ea0d6dfe943ae5db525
Michel Dumontier <michel.dumontier@gmail.com>
2018-02-08 16:11:29 -0400

    removed bio2rdf remnants

--- bio2rdf/website/bin/config.php
+++
@@ -1,5 +0,0 @@
-<?php 
-
-$servlet_url = "http://semanticscience.sharcnet.ca:48080/searchBio2rdf/search/SearchBio2rdf?query=";
-
-?>
--- bio2rdf/website/footer.php
+++
@@ -1,7 +0,0 @@
-        </div>
-        <div id="footer">
-                <p><a title="Creative Commons - By Attribution - Share-Alike" rel="license" href="http://creativecommons.org/licenses/by-sa/2"></a>
-        </div>
-</body>
-</html>        
-
